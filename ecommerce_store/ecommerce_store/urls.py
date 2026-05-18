9ffc65f8c336c87d5d3fbecf73e5356fc4f7f0a0
Jadeja-Pushprajsinh <officialjadejapushprajsinh@gmail.com>
2026-05-18 23:20:53 +0530

    home page of product listing  added

--- ecommerce_store/ecommerce_store/urls.py
+++ ecommerce_store/ecommerce_store/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from store.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('product/<int:id>/', product_detail),
 ]
